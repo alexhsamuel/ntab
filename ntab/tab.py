41ef86f10e6d3de8abb0043cfe648be666bff91a
Alex Samuel <alex@alexsamuel.net>
2017-05-06 12:43:22 -0400

    Split out lib.

--- ntab/tab.py
+++ ntab/tab.py
@@ -4,109 +4,16 @@ Pandas-lite tables, in a more numpy-oriented manner.
 
 #-------------------------------------------------------------------------------
 
-from   __future__ import absolute_import, division, print_function
+from   __future__ import absolute_import, division, print_function, unicode_literals
 
 import collections
+from   collections import OrderedDict as odict
 from   itertools import izip
 import numpy as np
 import sys
 
-try:
-    from cyordereddict import OrderedDict as odict
-except ImportError:
-    from collections import OrderedDict as odict
-
+from   .lib import *
 from   .np import argunique, arguniquen
-
-#-------------------------------------------------------------------------------
-# helper code
-
-def tupleize(obj):
-    """
-    Converts into or wraps in a tuple.
-
-    If `obj` is an iterable object other than a `str`, converts it to a `tuple`.
-      >>> tupleize((1, 2, 3))
-      (1, 2, 3)
-      >>> tupleize([1, 2, 3])
-      (1, 2, 3)
-      >>> tupleize(range(1, 4))
-      (1, 2, 3)
-
-    Otherwise, wraps `obj` in a one-element `tuple`.
-      >>> tupleize(42)
-      (42,)
-      >>> tupleize(None)
-      (None,)
-      >>> tupleize("Hello, world!")
-      ('Hello, world!',)
-
-    """
-    if isinstance(obj, basestring):
-        return (obj, )
-    else:
-        try:
-            return tuple(obj)
-        except:
-            return (obj, )
-
-
-def format_call(__name, *args, **kw_args):
-    """
-    Formats a function call as a string.  (Does not call any function.)
-
-      >>> import math
-      >>> format_call(math.hypot, 3, 4)
-      'hypot(3, 4)'
-
-      >>> format_call("myfunc", "arg", foo=42, bar=None)
-      "myfunc('arg', foo=42, bar=None)"
-
-    @param __name
-      The name of the function, or the function itself.
-    """
-    name = __name.__name__ if hasattr(__name, "__name__") else str(__name)
-    args = [ repr(a) for a in args ]
-    args.extend( n + "=" + repr(v) for n, v in kw_args.iteritems() )
-    return name + "(" + ", ".join(args) + ")"
-
-
-def format_ctor(obj, *args, **kw_args):
-    """
-    Formats an object construction as a string.
-
-    Like `format_call`, but uses the type name of `obj` as the function name.
-
-    Useful for quick-and-dirty `__repr__` implementations:
-
-      >>> class C:
-      ...     def __init__(self, foo):
-      ...         self.foo = foo
-      ...     def __repr__(self):
-      ...         return format_ctor(self, foo=self.foo)
-      ... 
-      >>> C(42)
-      C(foo=42)
-
-    """
-    return format_call(obj.__class__, *args, **kw_args)
-
-
-def sort_as(items, order):
-    items = list(items)
-    # First generate items from `order`, as long as they appear in `items`.
-    for item in order:
-        try:
-            i = items.index(item)
-        except ValueError:
-            pass
-        else:
-            yield item
-            del items[i]
-    # Then, generate any remaining items from `items`.
-    for item in items:
-        yield item
-
 
 #-------------------------------------------------------------------------------
 
