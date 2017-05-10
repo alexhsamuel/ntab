b58db844e0b8db37e9988ff2778791eddb0454e8
Alex Samuel <alex@alexsamuel.net>
2017-05-09 23:06:35 -0400

    Add basic setup.py.

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+setuptools.setup(
+    name="ntab",
+    version="0.0.0",
+    description="Simple numerical tables",
+    # long_description=long_description,
+    url="https://github.com/alexhsamuel/ntab",
+    author="Alex Samuel",
+    author_email="alex@alexsamuel.net",
+    license="MIT",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        # "Topic :: Software Development :: Build Tools",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+
+    # What does your project relate to?
+    keywords="table numerical",
+
+    packages=setuptools.find_packages(exclude=[]),
+
+    install_requires=["numpy"],
+
+    package_data={},
+    data_files=[],
+    entry_points={},
+)
+
